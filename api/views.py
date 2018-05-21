be7b3124075f86c46c7579f6d0b810a4a3310373
Evan Jarrett <ejfirestar00@gmail.com>
2018-05-21 00:36:39 -0700

    fix session login

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -52,6 +53,7 @@ class TokenAPIView(ObtainAuthToken):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
+        login(request, user)
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
